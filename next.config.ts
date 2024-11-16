b2495bc531f9d042eda280e3ab0482ec4bc402a0
nisim <nisimohana44@gmail.com>
2024-11-16 17:00:10 +0200

    Landing page Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wordpress-1366988-5037046.cloudwaysapps.com",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
